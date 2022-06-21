ce306aa0f8ed64da49d26c3d452e3dfea0353b9d
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-06-21 21:26:57 +0300

    empty class Square that defines a square:

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+Square = __import__('0-square').Square
 
-
-class Square:
-    '''Class Square
-    '''
-    pass
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)

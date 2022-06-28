f54138d77153fdd30f3f326ca847d00d736d8365
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-06-28 11:04:44 +0300

    Qns

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,75 +1,101 @@
 #!/usr/bin/python3
-"""This module contains an algorithm that resolves the N-Queen puzzle"""
 
-
-def isSafe(m_queen, nqueen):
-    """ Method that determines if the queens can or can't kill each other"""
-
-    for i in range(nqueen):
-
-        if m_queen[i] == m_queen[nqueen]:
-            return False
-
-        if abs(m_queen[i] - m_queen[nqueen]) == abs(i - nqueen):
-            return False
-
-    return True
-
-
-def print_result(m_queen, nqueen):
-    """ Method that prints the list with the Queens positions"""
-
-    res = []
-
-    for i in range(nqueen):
-        res.append([i, m_queen[i]])
-
-    print(res)
-
-
-def Queen(m_queen, nqueen):
-    """ Recursive function that executes the Backtracking algorithm"""
-
-    if nqueen is len(m_queen):
-        print_result(m_queen, nqueen)
-        return
-
-    m_queen[nqueen] = -1
-
-    while((m_queen[nqueen] < len(m_queen) - 1)):
-
-        m_queen[nqueen] += 1
-
-        if isSafe(m_queen, nqueen) is True:
-
-            if nqueen is not len(m_queen):
-                Queen(m_queen, nqueen + 1)
-
-
-def solveNQueen(size):
-    """ Function that invokes the Backtracking algorithm"""
-
-    m_queen = [-1 for i in range(size)]
-
-    Queen(m_queen, 0)
+"""
+This is a module for N queens.
+"""
 
 
 if __name__ == '__main__':
 
     import sys
+    import copy
 
-    if len(sys.argv) == 1 or len(sys.argv) > 2:
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         size = int(sys.argv[1])
-    except:
+    except BaseException:
         print("N must be a number")
         sys.exit(1)
-
     if size < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    solveNQueen(size)
+    def get_board(size):
+        """Returns an n by n board"""
+        board = [0] * size
+        for ix in range(size):
+            board[ix] = [0] * size
+        return board
+
+    def print_solutions(solutions, size):
+        """Prints all the solutions in user friendly way"""
+        new = []
+        for sol in solutions:
+            tmp = []
+            for i, row in enumerate(sol):
+                inner = []
+                for j, num in enumerate(row):
+                    if num == 1:
+                        inner.append(j)
+                        inner.append(i)
+                tmp.append(inner)
+            new.append(tmp)
+        for ans in new:
+            print(ans)
+
+    def is_safe(board, row, col, size):
+        """Check if it's safe to place a queen at board[x][y]"""
+
+        # check row on left side
+        for iy in range(col):
+            if board[row][iy] == 1:
+                return False
+
+        ix, iy = row, col
+        while ix >= 0 and iy >= 0:
+            if board[ix][iy] == 1:
+                return False
+            ix -= 1
+            iy -= 1
+
+        jx, jy = row, col
+        while jx < size and jy >= 0:
+            if board[jx][jy] == 1:
+                return False
+            jx += 1
+            jy -= 1
+
+        return True
+
+    def solve(board, col, size):
+        """Use backtracking to find all solutions"""
+        # base case
+        if col >= size:
+            return
+
+        for i in range(size):
+            if is_safe(board, i, col, size):
+                board[i][col] = 1
+                if col == size - 1:
+                    add_solution(board)
+                    board[i][col] = 0
+                    return
+                solve(board, col + 1, size)
+                # backtrack
+                board[i][col] = 0
+
+    def add_solution(board):
+        """Saves the board state to the global variable 'solutions'"""
+        global solutions
+        saved_board = copy.deepcopy(board)
+        solutions.append(saved_board)
+
+    board = get_board(size)
+    solutions = []
+    solve(board, 0, size)
+
+    print_solutions(solutions, size)
+
+    # print("Total solutions = {}".format(len(solutions)))
